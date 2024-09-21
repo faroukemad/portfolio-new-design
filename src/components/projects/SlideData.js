01f2b6d70e0133c64203dd6b88952ce077d44b11
faroukemad <faroukemad.pro@yahoo.com>
2024-09-21 15:17:10 +0300

    change img

--- src/components/projects/SlideData.js
+++ src/components/projects/SlideData.js
@@ -1,6 +1,6 @@
 const SlideData = [
   { imageSrc: 'assests/short.png', title: 'Shortlisted', link: 'https://shopshortlisted.com/' },
-  { imageSrc: 'assests/space.jpg', title: 'Tavola', link: 'https://space-adventure-blond.vercel.app/', githubLink: 'https://github.com/faroukemad/Space-Adventure' },
+  { imageSrc: 'assests/space.png', title: 'Tavola', link: 'https://space-adventure-blond.vercel.app/', githubLink: 'https://github.com/faroukemad/Space-Adventure' },
   { imageSrc: 'assests/word.jpg', title: 'WordPress', link: 'https://word-press-rouge-eight.vercel.app/', githubLink: 'https://github.com/faroukemad/wordPress' },
   { imageSrc: 'assests/tavola.jpg', title: 'Tavola', link: 'https://tavola-mu.vercel.app/', githubLink: 'https://github.com/faroukemad/Tavola' },
   { imageSrc: 'assests/store2.png', title: 'Online Store', link: 'https://store-zeta-eight.vercel.app/', githubLink: 'https://github.com/faroukemad/Store' },
